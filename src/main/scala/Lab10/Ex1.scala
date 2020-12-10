927fbff42d1312749674977b2822d8eda29bed59
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2020-12-10 09:55:36 +0200

    Laborator 10

--- src/main/scala/Lab10/Ex1.scala
+++ src/main/scala/Lab10/Ex1.scala
@@ -2,14 +2,35 @@ package Lab10
 
 import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
+import com.cra.figaro.library.compound._
+import com.cra.figaro.library.atomic.discrete._
+import com.cra.figaro.algorithm.factored._
+import com.cra.figaro.library.collection.Container
+import com.cra.figaro.library.atomic.continuous.Uniform
+import com.cra.figaro.library.atomic.continuous.Normal
+import com.cra.figaro.algorithm.sampling.Importance
 
-object Ex1 {
+object lab10 {
+	val capital: Array[Element[Double]] = Array.fill(10)(Constant(100))
+	val profit: Array[Element[Double]] = Array.fill(10)(Constant(0))
+	val investment: Array[Element[Double]] = Array.fill(10)(Constant(0))
+
+	def transition(previousCapital: Double): (Element[Double, Double, Double]) = {
+			val investment = previousCapital * 0.8
+			val profit = investment * 0.6
+			val newCapital = previousCapital + profit - investment
+			^^(investment, profit, newCapital)
+	} 
+
+  	for { i <- 1 until 10 } {
+	    val newState = Chain(capital(i), (c: Double) => transition(c))
+	    investment(i) = 
+	    profit(i) = 
+	    capital(i) = 
+  	}
+
 	def main(args: Array[String]) {
-		val test = Constant("Test")
-
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
+		for { i <- 1 until 10 } {	
+		}
 	}
 }
--- src/main/scala/Lab9/Ex1.scala
+++ src/main/scala/Lab9/Ex1.scala
@@ -1,46 +1,46 @@
-package Lab9
+// package Lab9
 
-import com.cra.figaro.language._
-import com.cra.figaro.algorithm.sampling._
-import com.cra.figaro.library.atomic.discrete.{FromRange, Poisson}
-import com.cra.figaro.algorithm.filtering.ParticleFilter
+// import com.cra.figaro.language._
+// import com.cra.figaro.algorithm.sampling._
+// import com.cra.figaro.library.atomic.discrete.{FromRange, Poisson}
+// import com.cra.figaro.algorithm.filtering.ParticleFilter
 
-object markovv {
-	val chapters = 10
-	val initial = Universe.createNew()
+// object markovv {
+// 	val chapters = 10
+// 	val initial = Universe.createNew()
 
-	Constant(3)("score", initial)
-	Constant(8)("learned", initial)
-	Constant(5)("difficulty", initial)
+// 	Constant(3)("score", initial)
+// 	Constant(8)("learned", initial)
+// 	Constant(5)("difficulty", initial)
 
-	def transition(learned: Int, difficulty: Int): (Element[(Int, Int, Int)]) = {
-			 ^^(newLearned, newScore, difficulty)
-	}
-	def nextUniverse(previous: Universe): Universe = {
-		val next = Universe.createNew()
-		val previousLearned = previous.get[Int]("learned")
-		val previousDifficulty = previous.get[Int]("difficulty")
-		val state = Chain(previousLearned, previousDifficulty, transition _)
-		Apply(state, (s: (Int, Int, Int)) => s._1)("learned", next)
-		Apply(state, (s: (Int, Int, Int)) => s._2)("score", next)
-		Apply(state, (s: (Int, Int, Int)) => s._3)("difficulty", next)
-		next 
-	}
+// 	def transition(learned: Int, difficulty: Int): (Element[(Int, Int, Int)]) = {
+// 			 ^^(newLearned, newScore, difficulty)
+// 	}
+// 	def nextUniverse(previous: Universe): Universe = {
+// 		val next = Universe.createNew()
+// 		val previousLearned = previous.get[Int]("learned")
+// 		val previousDifficulty = previous.get[Int]("difficulty")
+// 		val state = Chain(previousLearned, previousDifficulty, transition _)
+// 		Apply(state, (s: (Int, Int, Int)) => s._1)("learned", next)
+// 		Apply(state, (s: (Int, Int, Int)) => s._2)("score", next)
+// 		Apply(state, (s: (Int, Int, Int)) => s._3)("difficulty", next)
+// 		next 
+// 	}
 
-	def main(atgs: Array[String]){
-		val arrivingObservation = List(Some(1),Some(1),Some(1), None, None, None, None, None, None, None, None, None)
-		val alg = ParticleFilter(initial, nextUniverse, 100)
-		alg.start()
-		for { time <- 1 to 10} {
-			val evidence = {
-				arrivingObservation(time) match {
-					case None => List()
-					case Some(n) => List(NamedEvidence("score", Observation(n)))
-				}
-			}
-			alg.advanceTime(evidence)
-			print("Chapter" + time + ":")
-			println("expected learning = " + alg.currentExpectation("score", (i:Int) => i))
-		}
-	}
-}
+// 	def main(atgs: Array[String]){
+// 		val arrivingObservation = List(Some(1),Some(1),Some(1), None, None, None, None, None, None, None, None, None)
+// 		val alg = ParticleFilter(initial, nextUniverse, 100)
+// 		alg.start()
+// 		for { time <- 1 to 10} {
+// 			val evidence = {
+// 				arrivingObservation(time) match {
+// 					case None => List()
+// 					case Some(n) => List(NamedEvidence("score", Observation(n)))
+// 				}
+// 			}
+// 			alg.advanceTime(evidence)
+// 			print("Chapter" + time + ":")
+// 			println("expected learning = " + alg.currentExpectation("score", (i:Int) => i))
+// 		}
+// 	}
+// }
